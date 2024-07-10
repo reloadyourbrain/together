0b1fb679f2ebaa1b07945f6b4febb723b66f8c95
Reload <reloadyourbrain@gmail.com>
2024-07-10 16:26:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 999999))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
+OWNER_ID = int(getenv("OWNER_ID", 6051457085))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -40,7 +40,7 @@ GIT_TOKEN = getenv(
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/youramitty")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/poppunkgrup")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/bagaskarasupport")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
@@ -83,11 +83,11 @@ START_IMG_URL = getenv(
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph//file/782040e16d5a9824c915c.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph//file/782040e16d5a9824c915c.jpg"
 STATS_IMG_URL = "https://telegra.ph//file/782040e16d5a9824c915c.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph//file/782040e16d5a9824c915c.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph//file/782040e16d5a9824c915c.jpg"
+STREAM_IMG_URL = "https://telegra.ph//file/782040e16d5a9824c915c.jpg"
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
 YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
 SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
